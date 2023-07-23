012758bb6989b95850db749fc9e1bd89f72c3cd2
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-07-23 02:15:00 +0200

    Add trailing slashes to urls.py

--- modbus/urls.py
+++ modbus/urls.py
@@ -4,7 +4,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from modbus.core.views import StateAPIView
 
 urlpatterns = [
-    path("api/state", StateAPIView.as_view(), name="state"),
-    path("api/schema", SpectacularAPIView.as_view(), name="schema"),
-    path("api/schema/ui", SpectacularSwaggerView.as_view(), name="schema-ui"),
+    path("api/state/", StateAPIView.as_view(), name="state"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/ui/", SpectacularSwaggerView.as_view(), name="schema-ui"),
 ]
