f7e24e6209130fbafbd3a8214109e276635c7c9f
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-07-29 12:25:15 +0200

    Move credentials to .env file

---
+++ README.md
@@ -0,0 +1,19 @@
+# Deploying
+
+- Set up OS on ARM64 Raspberry Pi
+- Install Docker, Compose V2 should be installed by default
+- Clone this repository
+- Create new file `.env` in project directory
+- Add `DJANGO_SECRET_KEY` and `POSTGRES_PASSWORD`
+- Run `docker compose up -d --build --force-recreate`
+- Run `docker exec -it modbus-web-1 python /app/manage.py migrate`
+- Run `docker exec -it modbus-web-1 curl http://localhost:8000/api/state/`,
+  should return valid JSON data
+
+# Updating
+
+- Update the repository
+- Run `docker compose up -d --build --force-recreate`
+- Run `docker exec -it modbus-web-1 python /app/manage.py migrate`
+- Run `docker exec -it modbus-web-1 curl http://localhost:8000/api/state/`,
+  should return valid JSON data
--- modbus/settings.py
+++ modbus/settings.py
@@ -59,11 +59,11 @@ WSGI_APPLICATION = "modbus.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("POSTGRESQL_HOST", "localhost"),
-        "PORT": os.environ.get("POSTGRESQL_PORT", "5432"),
-        "NAME": os.environ.get("POSTGRESQL_NAME", "postgres"),
-        "USER": os.environ.get("POSTGRESQL_USER", "postgres"),
-        "PASSWORD": os.environ.get("POSTGRESQL_PASSWORD", "postgres"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+        "NAME": os.environ.get("POSTGRES_NAME", "postgres"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
     }
 }
 
