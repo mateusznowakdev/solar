457fe61499665756ba395226c18387f67aa5f3d9
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-07-29 10:19:12 +0200

    Mark schema API paths as hidden

--- modbus/urls.py
+++ modbus/urls.py
@@ -5,12 +5,13 @@ from modbus.core.views import MetaAPIView, SeriesAPIView, StateAPIView
 from modbus.front.views import ChartView, IndexView
 
 urlpatterns = [
+    # UI
     path("", IndexView.as_view(), name="front-index"),
     path("charts/", ChartView.as_view(), name="front-charts"),
-
-    path("api/meta/", MetaAPIView.as_view(), name="state"),
-    path("api/state/", StateAPIView.as_view(), name="state"),
-    path("api/series/", SeriesAPIView.as_view(), name="series"),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/schema/ui/", SpectacularSwaggerView.as_view(), name="schema-ui"),
+    # API
+    path("api/meta/", MetaAPIView.as_view(), name="api-state"),
+    path("api/state/", StateAPIView.as_view(), name="api-state"),
+    path("api/series/", SeriesAPIView.as_view(), name="api-series"),
+    path("api/_schema/", SpectacularAPIView.as_view(), name="api--schema"),
+    path("api/_schema/ui/", SpectacularSwaggerView.as_view(), name="api--schema-ui"),
 ]
