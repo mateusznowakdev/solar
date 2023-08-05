42c0117e5ce243dd7e6ce57c31517acd52a8db46
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-08-06 01:25:46 +0200

    Display log entries as cards

--- solar-ui/routes/Log.jsx
+++ solar-ui/routes/Log.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
-import { getBackendURI } from "../utils";
+import Card from "react-bootstrap/Card";
+
+import { METADATA } from "../meta";
 import { renderDateTime } from "../render";
+import { getBackendURI } from "../utils";
 
 export default function Log() {
   const [state, setState] = useState([]);
@@ -12,16 +15,22 @@ export default function Log() {
   }, []);
 
   return (
-    <div>
-      <ul>
-        {state.map((entry) => (
-          <li key={entry.timestamp}>
-            timestamp={renderDateTime(entry.timestamp)} field_name=
-            {entry.field_name} old_value={entry.old_value} new_value=
-            {entry.new_value}
-          </li>
-        ))}
-      </ul>
+    <div className="m-3">
+      {state.map((entry) => (
+        <Card className="mt-1 mb-2" key={entry.timestamp}>
+          <Card.Body>
+            <Card.Title as="h6">
+              {renderDateTime(new Date(entry.timestamp))}
+            </Card.Title>
+            <Card.Text>
+              {METADATA[entry.field_name].description}
+              <br />
+              {METADATA[entry.field_name].render(entry.old_value)} &rarr;{" "}
+              {METADATA[entry.field_name].render(entry.new_value)}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 }
