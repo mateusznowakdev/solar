4f54264fc89c5949a14808c2284e3b0f9f084ec8
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-08-06 01:25:34 +0200

    Increase number of returned log entries

--- solar/core/services/control_log.py
+++ solar/core/services/control_log.py
@@ -6,4 +6,4 @@ from solar.core.models import ControlLog
 class ControlLogService:
     @staticmethod
     def get_logs() -> QuerySet[ControlLog]:
-        return ControlLog.objects.all()[:10]
+        return ControlLog.objects.all()[:100]
