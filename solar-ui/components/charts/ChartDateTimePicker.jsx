b3c83c88e789ecc4b04e2d5f26d18391aa29f79a
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-08-20 00:07:17 +0200

    Switch back to the native date and time pickers

--- solar-ui/components/charts/ChartDateTimePicker.jsx
+++ solar-ui/components/charts/ChartDateTimePicker.jsx
@@ -1,54 +1,44 @@
 import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
-import ChartDateTimeStepper from "./ChartDateTimeStepper";
-
 export default function ChartDateTimePicker({ setValue, value }) {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    if (date && time) {
+      setValue(dayjs(date + time, "YYYY-MM-DDHH:mm"));
+    } else {
+      setValue(null);
+    }
+  }, [date, time]);
+
+  useEffect(() => {
+    if (value) {
+      setDate(dayjs(value).format("YYYY-MM-DD"));
+      setTime(dayjs(value).format("HH:mm"));
+    }
+  }, [value]);
+
   return (
-    <Form.Group as={Row} className="g-1 mb-2">
-      <Col xs={6}>
-        <ChartDateTimeStepper
-          decValue={() => setValue(dayjs(value).subtract(1, "day"))}
-          incValue={() => setValue(dayjs(value).add(1, "day"))}
-          setValue={() => {}}
-          value={new Intl.DateTimeFormat(undefined, {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-            weekday: "short",
-          }).format(value)}
+    <Form.Group as={Row} className="g-2 mb-2">
+      <Col xs={7}>
+        <Form.Control
+          onChange={(e) => setDate(e.target.value)}
+          required
+          type="date"
+          value={date}
         />
       </Col>
-      <Col xs={2}>
-        <ChartDateTimeStepper
-          decValue={() => setValue(dayjs(value).subtract(1, "hour"))}
-          incValue={() => setValue(dayjs(value).add(1, "hour"))}
-          setValue={(h) => setValue(dayjs(value).hour(h))}
-          value={new Intl.DateTimeFormat(undefined, {
-            hour: "2-digit",
-          }).format(value)}
-        />
-      </Col>
-      <Col xs={2}>
-        <ChartDateTimeStepper
-          decValue={() => setValue(dayjs(value).subtract(1, "minute"))}
-          incValue={() => setValue(dayjs(value).add(1, "minute"))}
-          setValue={(m) => setValue(dayjs(value).minute(m))}
-          value={new Intl.DateTimeFormat(undefined, {
-            minute: "2-digit",
-          }).format(value)}
-        />
-      </Col>
-      <Col xs={2}>
-        <ChartDateTimeStepper
-          decValue={() => setValue(dayjs(value).subtract(1, "second"))}
-          incValue={() => setValue(dayjs(value).add(1, "second"))}
-          setValue={(s) => setValue(dayjs(value).second(s))}
-          value={new Intl.DateTimeFormat(undefined, {
-            second: "2-digit",
-          }).format(value)}
+      <Col xs={5}>
+        <Form.Control
+          onChange={(e) => setTime(e.target.value)}
+          required
+          type="time"
+          value={time}
         />
       </Col>
     </Form.Group>
