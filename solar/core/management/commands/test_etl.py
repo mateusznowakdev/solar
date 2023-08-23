933a0c803e3383fc81c31b2b46ec767e744d1ec8
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-08-24 01:11:29 +0200

    Move ETL code to services module

--- solar/core/management/commands/test_etl.py
+++ solar/core/management/commands/test_etl.py
@@ -1,8 +1,8 @@
 from django.core.management import BaseCommand
 from django.db import connection
 
-from solar.core import etl
 from solar.core.models import StateArchive, StateT1, StateT2, StateT3, StateT4
+from solar.core.services import transform
 
 
 class Command(BaseCommand):
@@ -12,17 +12,17 @@ class Command(BaseCommand):
         # TODO: process StateArchive every ...
         # TODO: process StateRaw every ...
 
-        etl.process_data(StateT1)
-        etl.process_data(StateT2)
-        etl.process_data(StateT3)
-        etl.process_data(StateT4)
-        etl.process_data(StateArchive)
+        transform.process_data(StateT1)
+        transform.process_data(StateT2)
+        transform.process_data(StateT3)
+        transform.process_data(StateT4)
+        transform.process_data(StateArchive)
 
-        etl.delete_data(StateT1)
-        etl.delete_data(StateT2)
-        etl.delete_data(StateT3)
-        etl.delete_data(StateT4)
-        etl.delete_data_staging()
+        transform.delete_data(StateT1)
+        transform.delete_data(StateT2)
+        transform.delete_data(StateT3)
+        transform.delete_data(StateT4)
+        transform.delete_data_staging()
 
         for q in connection.queries:
             print(q["sql"])
