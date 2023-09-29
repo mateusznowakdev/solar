d63fdac9d4d9a046d1bf532667ca772085387ac2
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-09-29 22:06:26 +0200

    Disable schema view in production mode

--- solar/urls.py
+++ solar/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -6,6 +7,10 @@ from solar.core.views import LogAPIView, SeriesAPIView
 urlpatterns = [
     path("api/log/", LogAPIView.as_view(), name="api-log"),
     path("api/series/", SeriesAPIView.as_view(), name="api-series"),
-    path("api/_schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/_schema/ui/", SpectacularSwaggerView.as_view(), name="schema-ui"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("api/_schema/", SpectacularAPIView.as_view(), name="schema"),
+        path("api/_schema/ui/", SpectacularSwaggerView.as_view(), name="schema-ui"),
+    ]
