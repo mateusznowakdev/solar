0b0c37da4b1211bcca3d260a16a8fd7da8d9c720
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2024-02-27 00:15:12 +0100

    Refactor filter buttons

---
+++ solar-ui/components/log/LogFilter.jsx
@@ -0,0 +1,23 @@
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+
+export default function LogFilter({
+  name,
+  description,
+  filters,
+  toggleFilter,
+}) {
+  return (
+    <ButtonGroup className="me-2">
+      <Button
+        className="p-2"
+        onClick={() => toggleFilter(name)}
+        size="sm"
+        variant="light"
+      >
+        {filters.includes(name) && <Check size={16} />}
+        {description}
+      </Button>
+    </ButtonGroup>
+  );
+}
--- solar-ui/components/log/LogFilters.jsx
+++ solar-ui/components/log/LogFilters.jsx
@@ -1,46 +1,29 @@
-import Check from "lucide-react/dist/esm/icons/check";
-import Button from "react-bootstrap/Button";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 
 import { STRINGS } from "../../locale";
+import LogFilter from "./LogFilter";
 
 export default function LogFilters({ filters, toggleFilter }) {
   return (
     <ButtonToolbar className="pb-2">
-      <ButtonGroup className="me-2">
-        <Button
-          className="px-2"
-          onClick={() => toggleFilter("errors")}
-          size="sm"
-          variant="light"
-        >
-          {filters.includes("errors") && <Check size={16} />}
-          {STRINGS.ERRORS}
-        </Button>
-      </ButtonGroup>
-      <ButtonGroup className="me-2">
-        <Button
-          className="px-2"
-          onClick={() => toggleFilter("automation")}
-          size="sm"
-          variant="light"
-        >
-          {filters.includes("automation") && <Check size={16} />}
-          {STRINGS.AUTOMATION}
-        </Button>
-      </ButtonGroup>
-      <ButtonGroup className="me-2">
-        <Button
-          className="px-2"
-          onClick={() => toggleFilter("system")}
-          size="sm"
-          variant="light"
-        >
-          {filters.includes("system") && <Check size={16} />}
-          {STRINGS.SYSTEM}
-        </Button>
-      </ButtonGroup>
+      <LogFilter
+        name="errors"
+        description={STRINGS.ERRORS}
+        filters={filters}
+        toggleFilter={toggleFilter}
+      />
+      <LogFilter
+        name="automation"
+        description={STRINGS.AUTOMATION}
+        filters={filters}
+        toggleFilter={toggleFilter}
+      />
+      <LogFilter
+        name="system"
+        description={STRINGS.SYSTEM}
+        filters={filters}
+        toggleFilter={toggleFilter}
+      />
     </ButtonToolbar>
   );
 }
