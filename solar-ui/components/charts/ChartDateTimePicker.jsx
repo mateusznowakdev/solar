b70107bcf0017328c3eed635662e34e7339ed5a2
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2024-03-01 21:52:27 +0100

    Add extra buttons in date-time picker

--- solar-ui/components/charts/ChartDateTimePicker.jsx
+++ solar-ui/components/charts/ChartDateTimePicker.jsx
@@ -1,8 +1,11 @@
 import dayjs from "dayjs";
+import ChevronLeft from "lucide-react/dist/esm/icons/chevron-left";
+import ChevronRight from "lucide-react/dist/esm/icons/chevron-right";
+import X from "lucide-react/dist/esm/icons/x";
 import { useEffect, useState } from "react";
-import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
+import InputGroup from "react-bootstrap/InputGroup";
 
 export default function ChartDateTimePicker({ setValue, value }) {
   const [date, setDate] = useState("");
@@ -23,24 +26,39 @@ export default function ChartDateTimePicker({ setValue, value }) {
     }
   }, [value]);
 
+  function addDay() {
+    setDate(dayjs(date).add(1, "day").format("YYYY-MM-DD"));
+  }
+
+  function subDay() {
+    setDate(dayjs(date).subtract(1, "day").format("YYYY-MM-DD"));
+  }
+
   return (
-    <Form.Group as={Row} className="g-2 mb-2">
-      <Col xs={7}>
-        <Form.Control
-          onChange={(e) => setDate(e.target.value)}
-          required
-          type="date"
-          value={date}
-        />
-      </Col>
-      <Col xs={5}>
-        <Form.Control
-          onChange={(e) => setTime(e.target.value)}
-          required
-          type="time"
-          value={time}
-        />
-      </Col>
-    </Form.Group>
+    <InputGroup className="mb-2">
+      <Button onClick={subDay} variant="light">
+        <ChevronLeft strokeWidth={1.25} />
+      </Button>
+      <Form.Control
+        onChange={(e) => setDate(e.target.value)}
+        required
+        style={{ flex: 3 }}
+        type="date"
+        value={date}
+      />
+      <Button onClick={addDay} variant="light">
+        <ChevronRight strokeWidth={1.25} />
+      </Button>
+      <Form.Control
+        onChange={(e) => setTime(e.target.value)}
+        required
+        style={{ flex: 2 }}
+        type="time"
+        value={time}
+      />
+      <Button onClick={() => setTime("00:00")} variant="light">
+        <X strokeWidth={1.25} />
+      </Button>
+    </InputGroup>
   );
 }
