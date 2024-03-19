38d15409179a33adef61008d44a259f31852cd2d
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2024-03-19 18:53:07 +0100

    Restore, but comment out build statements in Compose file

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-set -xe
+set -e
 
 git pull
 
-docker compose --parallel 1 up -d --build
+docker compose --parallel 1 up -d --pull always --build
 sleep 5
 docker exec -it solar-backend-1 python /app/manage.py migrate
+
+echo -e "\e[42mApplication has been updated successfully\e[0m"
