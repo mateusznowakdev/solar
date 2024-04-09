b5d50d9bbf0f32b77f38b88143fdcd91f41cf30c
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2024-04-10 00:24:12 +0200

    Access frontend container via separate nginx container

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
-import gzipPlugin from "rollup-plugin-gzip";
 import loadVersion from "vite-plugin-package-version";
 
 export default defineConfig({
-  plugins: [react(), loadVersion(), gzipPlugin({ filter: /^assets\/.*/ })],
+  plugins: [react(), loadVersion()],
   root: "solar-ui",
 });
