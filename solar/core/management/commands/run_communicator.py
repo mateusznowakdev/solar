10344864d5dfe24e68be0a22bde1813a747d280e
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2025-01-06 00:28:00 +0100

    Run minimal HTTP server in communicator service

--- solar/core/management/commands/run_communicator.py
+++ solar/core/management/commands/run_communicator.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import traceback
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 from django.core.management import BaseCommand
 
@@ -10,27 +11,37 @@ from solar.core.services.publish import PublishService
 DEVICE_LIST = ("/dev/ttyUSB0", "/dev/ttyUSB1", "/dev/ttyUSB2", "/dev/ttyUSB3")
 
 
-def run_communicator(control_service, publish_service):
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        publish_service = PublishService()
+        http_server = HTTPServer(("0.0.0.0", 8000), HTTPRequestHandler)
+        http_server.timeout = 0.5
+
+        for device in DEVICE_LIST:
+            try:
+                control_service = ControlService(device=device)
+                run_communicator(control_service, publish_service, http_server)
+            except Exception as e:  # pylint:disable=broad-exception-caught
+                traceback.print_exception(e)
+                time.sleep(2.5)
+
+        sys.exit(1)
+
+
+def run_communicator(control_service, publish_service, http_server):
     while True:
+        http_server.handle_request()
+
         state = control_service.get_state()
 
         control_service.save_state(state=state)
         control_service.postprocess_state(state=state)
         publish_service.publish(state=state)
 
-        time.sleep(0.75)
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        publish_service = PublishService()
-
-        for device in DEVICE_LIST:
-            try:
-                control_service = ControlService(device=device)
-                run_communicator(control_service, publish_service)
-            except Exception as e:  # pylint:disable=broad-exception-caught
-                traceback.print_exception(e)
-                time.sleep(2.5)
-
-        sys.exit(1)
+        time.sleep(0.5)
