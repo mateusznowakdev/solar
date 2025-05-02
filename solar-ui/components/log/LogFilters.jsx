6ac8f7c1f3a947071564d3ed87630305abdedeca
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2025-05-03 01:18:17 +0200

    Add check icon for selected filters

--- solar-ui/components/log/LogFilters.jsx
+++ solar-ui/components/log/LogFilters.jsx
@@ -1,16 +1,18 @@
+import Check from "lucide-react/dist/esm/icons/check";
 import Badge from "react-bootstrap/Badge";
 import Stack from "react-bootstrap/Stack";
 
 import { STRINGS } from "../../locale";
 
-function LogFilter({ name, description, filters, toggleFilter }) {
+function LogFilter({ name, description, checked, toggleFilter }) {
   return (
     <Badge
       bg=""
-      className={"py-2 " + (filters.includes(name) ? "success" : "")}
+      className={"py-2 " + (checked ? "success" : "")}
       onClick={() => toggleFilter(name)}
       pill
     >
+      {checked && <Check size={16} />}
       {description}
     </Badge>
   );
@@ -22,19 +24,19 @@ export default function LogFilters({ filters, toggleFilter }) {
       <LogFilter
         name="errors"
         description={STRINGS.ERRORS}
-        filters={filters}
+        checked={filters.includes("errors")}
         toggleFilter={toggleFilter}
       />
       <LogFilter
         name="automation"
         description={STRINGS.AUTOMATION}
-        filters={filters}
+        checked={filters.includes("automation")}
         toggleFilter={toggleFilter}
       />
       <LogFilter
         name="system"
         description={STRINGS.SYSTEM}
-        filters={filters}
+        checked={filters.includes("system")}
         toggleFilter={toggleFilter}
       />
     </Stack>
